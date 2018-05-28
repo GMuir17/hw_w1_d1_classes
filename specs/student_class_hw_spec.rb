9bf2b3a444d5eabfb57d185b27cfd76c6a6e7247
Gary Muir <garymuir17@gmail.com>
2018-05-28 15:45:04 +0100

    setter methods added and tested

--- specs/student_class_hw_spec.rb
+++ specs/student_class_hw_spec.rb
@@ -3,7 +3,7 @@ require_relative("../student_class_hw.rb")
 
 class StudentClassTest < MiniTest::Test
 
-  def test_get_student_name
+  def test_get_name
     student = CodeClanStudent.new("Gary", "E22")
     assert_equal("Gary", student.get_name())
   end
@@ -13,9 +13,15 @@ class StudentClassTest < MiniTest::Test
     assert_equal("E22", student.get_cohort())
   end
 
+  def test_set_student
+    student = CodeClanStudent.new("Gary", "E22")
+    assert_equal("Wil", student.set_name("Wil"))
+  end
 
-
-
+  def test_set_cohort
+    student = CodeClanStudent.new("Gary", "E22")
+    assert_equal("E19", student.set_cohort("E19"))
+  end
 
 
 
