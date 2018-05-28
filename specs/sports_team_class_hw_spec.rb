53c2b49a224608ab194ddce54092558780f69be4
Gary Muir <garymuir17@gmail.com>
2018-05-28 16:13:17 +0100

    refactored with attr_reader and attr_writer

--- specs/sports_team_class_hw_spec.rb
+++ specs/sports_team_class_hw_spec.rb
@@ -5,22 +5,22 @@ class SportsTeamClassTest < Minitest::Test
 
   def test_get_team_name
     team = SportTeam.new("The Triumverate", ["Gaius", "Marc", "Crassus", "Pompey"], "Julius")
-    assert_equal("The Triumverate", team.get_team_name())
+    assert_equal("The Triumverate", team.team_name)
   end
 
   def test_get_players
     team = SportTeam.new("The Triumverate", ["Gaius", "Marc", "Crassus", "Pompey"], "Julius")
-    assert_equal(["Gaius", "Marc", "Crassus", "Pompey"], team.get_players())
+    assert_equal(["Gaius", "Marc", "Crassus", "Pompey"], team.players)
   end
 
   def test_get_coach
     team = SportTeam.new("The Triumverate", ["Gaius", "Marc", "Crassus", "Pompey"], "Julius")
-    assert_equal("Julius", team.get_coach())
+    assert_equal("Julius", team.coach)
   end
 
   def test_set_coach
     team = SportTeam.new("The Triumverate", ["Gaius", "Marc", "Crassus", "Pompey"], "Julius")
-    assert_equal("Brutus", team.set_coach("Brutus"))
+    assert_equal("Brutus", team.coach = "Brutus")
   end
 
 
